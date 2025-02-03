c797b71a3a9a5604a91f9344eebd9b361d94ff1a
AmauryLAPEYRE <amos.lapeyre@gmail.com>
2025-02-03 14:18:30 +0100

    Modification

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/calculateur-heures/',
   assetsInclude: ['**/*.xlsx'],
   server: {
     fs: {
